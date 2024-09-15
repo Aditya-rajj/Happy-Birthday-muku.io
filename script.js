ab54948ca2d633fd3e35030a32c649ada30186b9
Aditya raj <149693721+Aditya-rajj@users.noreply.github.com>
2024-09-15 20:22:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function showMessage() {
+    const messageElement = document.getElementById('specialMessage');
+    messageElement.textContent = "Jyada khush mat ho  baby abhi v tumko mera sara baat manna padega 😁";
+    messageElement.classList.toggle('hidden');
+}
